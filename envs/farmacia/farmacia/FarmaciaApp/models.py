a20532d5f9804234c1a620dd85abd100eb032c4d
Florencia Galan <vallegalan810@gmail.com>
2022-10-31 05:54:37 -0300

    configuracion del super usuario para que se loguee con email

--- envs/farmacia/farmacia/FarmaciaApp/models.py
+++ envs/farmacia/farmacia/FarmaciaApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+ #Create your models here.
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
--- envs/farmacia/farmacia/farmacia/urls.py
+++ envs/farmacia/farmacia/farmacia/urls.py
@@ -36,9 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #Documentacion de la API para ver el swagger
-    
-    #http://127.0.0.1:8000/swagger/?format=openapi
+    #Documentacion de la API para ver el swagger   #http://127.0.0.1:8000/swagger/?format=openapi
     #path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
