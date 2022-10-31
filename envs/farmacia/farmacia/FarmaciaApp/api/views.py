4064c8db31381c503cd7bfe32bba4b3f1c3d8bf9
Florencia Galan <vallegalan810@gmail.com>
2022-10-31 06:40:34 -0300

    contraseña encriptada

--- envs/farmacia/farmacia/FarmaciaApp/api/views.py
+++ envs/farmacia/farmacia/FarmaciaApp/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
-
+from django.contrib.auth.hashers import make_password
 
 from FarmaciaApp.models import User
 from FarmaciaApp.api.serializers import UserSerializer
@@ -10,3 +10,7 @@ class UserApiViewSet(ModelViewSet):
     permission_class = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all() 
+    #Encriptar la contraseña que se guarda en la BD
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
